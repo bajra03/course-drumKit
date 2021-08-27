92016c8fbbabd762d40f91c44109b829251535dd
bajra <bajra.idabagus@karmagroup.com>
2021-08-27 16:09:24 +0800

    update function

--- index.js
+++ index.js
@@ -1,50 +1,7 @@
 const numberOfButton = document.querySelectorAll(".drum").length;
-const drums = document.querySelectorAll('.drum')
-const tom1 = new Audio('sounds/tom-1.mp3');
-const tom2 = new Audio('sounds/tom-2.mp3');
-const tom3 = new Audio('sounds/tom-3.mp3');
-const tom4 = new Audio('sounds/tom-4.mp3');
-const crash = new Audio('sounds/crash.mp3');
-const kickBass = new Audio('sounds/kick-bass.mp3');
-const snare = new Audio('sounds/snare.mp3');
-
-function makeSound(key) {
-  switch (key) {
-    case "w":
-        tom1.play();
-      break;
-    
-    case "a":
-        tom2.play();
-      break;
-    
-    case "s":
-      tom3.play();
-      break;
-    
-    case "d":
-      tom4.play();
-      break;
-    
-    case "j":
-      crash.play();
-      break;
-    
-    case "k":
-      kickBass.play();
-      break;
-    
-    case "l":
-      snare.play();
-      break;
-  
-    default:
-      break;
-  }
-}
 
 for (let i = 0; i < numberOfButton; i++) {
-  drums[i].addEventListener("click", function () {
+  document.querySelectorAll('.drum')[i].addEventListener("click", function () {
     const buttonText = this.textContent;
     makeSound(buttonText);
     buttonAnimation(buttonText);
@@ -55,6 +12,48 @@ document.addEventListener('keypress', function (e) {
   makeSound(e.key);
   buttonAnimation(e.key);
 });
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      const tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+      break;
+    
+    case "a":
+      const tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+      break;
+    
+    case "s":
+      const tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    
+    case "d":
+      const tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    
+    case "j":
+      const crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    
+    case "k":
+      const kickBass = new Audio('sounds/kick-bass.mp3');
+      kickBass.play();
+      break;
+    
+    case "l":
+      const snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+  
+    default:
+      break;
+  }
+}
 
 function buttonAnimation(key) {
   const activeButton = document.querySelector(`.${key}`);
